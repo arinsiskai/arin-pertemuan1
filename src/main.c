a53c209116c6990078468ea01a34eb82fa880d45
arinsikai <arinsiska@gmail.com>
2019-04-09 07:52:39 +0700

    test for first commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int y, x,jumlah;
+    y = 6;
+    x = 7;
+    char huruf;
+    huruf = 'c';
+    jumlah = y+x;
+    printf("huruf = %c", huruf);
+    return 0;
+} 
